d0ae704a17e7b93d4af31437285404a57c8449fa
Adam Lerer <adam.lerer@gmail.com>
2009-07-01 20:41:15 +0000

    Renamed OdkManage->OdkManageService

--- mobile/src/org/google/android/odk/manage/OdkManageService.java
+++ mobile/src/org/google/android/odk/manage/OdkManageService.java
@@ -4,7 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class OdkManage extends Service{
+public class OdkManageService extends Service{
 
   @Override
   public IBinder onBind(Intent intent) {
