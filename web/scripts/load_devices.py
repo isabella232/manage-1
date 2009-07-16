f40fba86cdc185373c20bdd5e9159e7b89fd2549
Adam Lerer <adam.lerer@gmail.com>
2009-07-16 18:47:21 +0000

    Some python scripts for managing the data. To use the appengine_console and delete_tasks scripts, you have to run appcfg.py upload on the scripts directory, with app.yaml set to your application name. Then you can use the scripts, which work with remote_api.

---
+++ web/scripts/load_devices.py
@@ -0,0 +1,44 @@
+import urllib
+import urllib2
+
+domain = 'http://odk-manage-stresstest.appspot.com'
+
+def loadDevicesAndTasks(numDevices, numTasks, offset=0):
+  for n in range(offset, offset+numDevices):
+    imei = loadDevice(n)
+    loadTasks(imei, numTasks)
+    
+def loadDevice(n):
+  url = domain + '/register'
+  imei = str(1000000 + n)
+  values = {'imei' : imei,
+            'userid' : 'User' + str(n),
+            'phonenumber' : '+123456' + str(n),
+            'sim' : str(n),
+            'imsi' : str(n) }
+  
+  doPost(url,values)
+  print 'Loaded device ' + str(n)
+  return imei
+    
+def loadTasks(imei, numTasks):
+  for n in range (0, numTasks):
+    url = domain + '/addTask'
+    values = {'adminToken' : 'ureport',
+              'imei' : imei,
+              'type' : 'ADD_FORM',
+              'name' : 'form' + str(n),
+              'url' : 'http://www.example.com/form/' + str(n) }
+    doPost(url,values)
+    
+
+    
+def doPost(url, values):
+  try:
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    urllib2.urlopen(req)
+  except:
+    print 'Error'
+
+loadDevicesAndTasks(10, 10, offset=0)
