ed4fdd307bf42fc52a8ec2c64b601c400685a0f8
Adam Lerer <adam.lerer@gmail.com>
2009-07-23 23:07:19 +0000

    Added hard check to make sure that SMS is ONLY sent when enabled.

--- mobile/src/org/odk/manage/android/comm/SmsSender.java
+++ mobile/src/org/odk/manage/android/comm/SmsSender.java
@@ -1,13 +1,5 @@
 package org.odk.manage.android.comm;
 
-import org.odk.manage.android.Constants;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,14 +7,23 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.telephony.gsm.SmsManager;
 import android.util.Log;
-import android.widget.Toast;
+
+import org.odk.manage.android.Constants;
+import org.odk.manage.android.SharedPreferencesAdapter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class SmsSender {
   
   private Context ctx;
+  private SharedPreferencesAdapter prefsAdapter;
   
   public SmsSender(Context ctx){
     this.ctx = ctx;
+    this.prefsAdapter = new SharedPreferencesAdapter(ctx);
   }
   
   private static int messageIndex = 0;
@@ -34,24 +35,33 @@ public class SmsSender {
   //---sends an SMS message to another device---
   public void sendSMS(String phoneNumber, String message, BroadcastReceiver onSent, BroadcastReceiver onDelivered)
   {        
-      messageIndex++;
-      String DELIVERED = "SMS_DELIVERED_" + messageIndex;
-      String SENT = "SMS_SENT_" + messageIndex;
       
-      PendingIntent sentPI = PendingIntent.getBroadcast(ctx, 0,
-          new Intent(SENT), 0);
-      ctx.registerReceiver(onSent, new IntentFilter(SENT));   
-      
-      PendingIntent deliveredPI = PendingIntent.getBroadcast(ctx, 0,
-          new Intent(DELIVERED), 0);
-      ctx.registerReceiver(onDelivered, new IntentFilter(DELIVERED));   
+    /**
+     * This is a hard check to make sure that SMS's are NEVER sent if SMS 
+     * sending is disabled.
+     */
+    if (prefsAdapter.getBoolean(Constants.PREF_SMS_ENABLED_KEY, false)){
+      Log.e(Constants.TAG, "Application tried to send SMS while SMS was disabled.");
+      return;
+    }
+    messageIndex++;
+    String DELIVERED = "SMS_DELIVERED_" + messageIndex;
+    String SENT = "SMS_SENT_" + messageIndex;
+    
+    PendingIntent sentPI = PendingIntent.getBroadcast(ctx, 0,
+        new Intent(SENT), 0);
+    ctx.registerReceiver(onSent, new IntentFilter(SENT));   
+    
+    PendingIntent deliveredPI = PendingIntent.getBroadcast(ctx, 0,
+        new Intent(DELIVERED), 0);
+    ctx.registerReceiver(onDelivered, new IntentFilter(DELIVERED));   
 
-      
-      SmsManager sms = SmsManager.getDefault();
-      String fullMessage =  Constants.MANAGE_SMS_TOKEN + " " + message;
-      
-      sms.sendTextMessage(phoneNumber, null, fullMessage, sentPI, deliveredPI); 
-      Log.i("OdkManage", "Sms sent: Recipient: " + phoneNumber + "; Message: " + fullMessage);
+    
+    SmsManager sms = SmsManager.getDefault();
+    String fullMessage =  Constants.MANAGE_SMS_TOKEN + " " + message;
+    
+    sms.sendTextMessage(phoneNumber, null, fullMessage, sentPI, deliveredPI); 
+    Log.i("OdkManage", "Sms sent: Recipient: " + phoneNumber + "; Message: " + fullMessage);
   }
   
   private String createSmsWithParameters(String action, Map<String,String> parameters){
@@ -73,35 +83,3 @@ public class SmsSender {
   }
   
 }
-
-
-//---when the SMS has been sent---
-//ctx.registerReceiver(new BroadcastReceiver(){
-//  @Override
-//  public void onReceive(Context arg0, Intent arg1) {
-//      switch (getResultCode())
-//      {
-//          case Activity.RESULT_OK:
-//              Toast.makeText(ctx, "SMS sent", 
-//                      Toast.LENGTH_SHORT).show();
-//              break;
-//          case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-//              Toast.makeText(ctx, "Generic failure", 
-//                      Toast.LENGTH_SHORT).show();
-//              break;
-//          case SmsManager.RESULT_ERROR_NO_SERVICE:
-//              Toast.makeText(ctx, "No service", 
-//                      Toast.LENGTH_SHORT).show();
-//              break;
-//          case SmsManager.RESULT_ERROR_NULL_PDU:
-//              Toast.makeText(ctx, "Null PDU", 
-//                      Toast.LENGTH_SHORT).show();
-//              break;
-//          case SmsManager.RESULT_ERROR_RADIO_OFF:
-//              Toast.makeText(ctx, "Radio off", 
-//                      Toast.LENGTH_SHORT).show();
-//              break;
-//      }
-//  }
-//}, new IntentFilter(SENT));
-//
